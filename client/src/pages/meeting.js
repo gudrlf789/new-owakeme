ef18f09183a4045109c6b09b306e7b8ad33240f8
park hyeong gil <gudrlf7913@naver.com>
2021-10-12 11:21:51 +0900

    미팅 화면 코드 수정

--- client/src/pages/meeting.js
+++ client/src/pages/meeting.js
@@ -1,36 +1,28 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
-import StreamPlayer from '../components/streamPlayer';
-import NavBar from '../components/navBar';
+import { Button, Grid, useMediaQuery } from '@material-ui/core';
+import StreamPlayer from '../components/streamPlayer_test';
 import AgoraRTC from 'agora-rtc-sdk-ng';
 import RTCClient from '../rtc-client';
 import { IonIcon } from '@ionic/react'
-import { micOutline, micOffOutline, videocamOutline, videocamOffOutline, shareOutline, logOutOutline } from 'ionicons/icons'
+import { menuOutline, micOutline, micOffOutline, videocamOutline, videocamOffOutline, shareOutline, logOutOutline } from 'ionicons/icons'
 import useRouter from '../utils/use-router';
 import axios from 'axios';
 import { userLogOut } from '../reducer/actions/user';
-import { dontUseSharing } from '../reducer/actions/track';
+import { dontUseSharing, setCurrentTrack } from '../reducer/actions/track';
 import "../assets/css/meeting.css";
 import "../assets/css/navigator.css";
 import new_owake_logo_white from "../assets/img/new_owake_logo_white.svg";
 import setting from '../assets/img/settings.svg';
-import owake_icon_mic from '../assets/img/owake_icon_mic.png';
-import owake_icon_cam from '../assets/img/owake_icon_cam.png';
-import owake_icon_close from '../assets/img/owake_icon_close.png';
-import owake_icon_share from '../assets/img/owake_icon_share.png';
-import disable_mic from '../assets/img/disable_mic.png';
-import disable_cam from '../assets/img/disable_cam.png';
-import disable_share from '../assets/img/disable_share.png';
-import Chatting from '../subcomponents/chatting';
 
 const Room = () => {
   const routerCtx = useRouter();
-  const { sharing } = useSelector(state => state.trackReducer);
+  const { sharing, tracks, currentTrack } = useSelector(state => state.trackReducer);
   const [useMic, setUseMic] = useState(true);
   const [useVideocam, setUseVideocam] = useState(true);
   const [useSetting, setUseSetting] = useState(false);
+  const [mainTrack, setMainTrack] = useState([]);
   const dispatch = useDispatch();
 
   const client = useMemo(() => {
@@ -38,105 +30,123 @@ const Room = () => {
   }, [])
 
   const {
-    localVideoTrack, localAudioTrack, remoteUsers, localUid, share, leave
+    share, leave
   } = RTCClient(client);
   
-  const onMic = () => {
+  let otherTracks = useMemo(() => {
+    if(mainTrack.length > 0){
+      return tracks.filter(tracks => tracks.uid != mainTrack[0].uid);
+    }
+    return tracks.filter(tracks => tracks.uid != currentTrack[0].uid);
+  }, [currentTrack, tracks, mainTrack]);
+  
+  const onMic = useCallback(() => {
     setUseMic(!useMic)
-    localAudioTrack.setMuted(useMic)
-  }
+    currentTrack[0].audioTrack.setMuted(useMic)
+  }, [useMic, currentTrack])
 
-  const onVideocam = () => {
+  const onVideocam = useCallback(() => {
     setUseVideocam(!useVideocam)
-    localVideoTrack.setMuted(useVideocam)
-  }
+    currentTrack[0].videoTrack.setMuted(useVideocam)
+  }, [useVideocam, currentTrack]);
 
   const onShareScreen = async function() {
     dispatch(dontUseSharing());
     await share();
   }
 
-  const onLeaveChannel = () => {
-    leave(localVideoTrack, localAudioTrack, client);
+  const onLeaveChannel = useCallback(() => {
+    leave(currentTrack[0].videoTrack, currentTrack[0].audioTrack, client);
     dispatch(userLogOut());
     routerCtx.history.push({ pathname: '/' });
-  }
+  }, [tracks]);
 
   const onUseSetting = useCallback(() => {
     setUseSetting(!useSetting);
   }, [useSetting]);
 
+  const onChangeMainTrack = useCallback((uid) => {
+    setMainTrack(tracks.filter(tracks => tracks.uid == uid));
+  }, [mainTrack, tracks]);
+
+  const toggleMenu = useCallback(() => {
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+  }, [])
+  
   window.onunload = function() {
     onLeaveChannel();
-
     return "";
   };	    	
 
   return (
-    <> 
-      <div className="video_container">
+    <div class="container">
+      <div class="navigation">
+        <ul>
+          <li>
+            <span class="title"><img src={new_owake_logo_white} alt="" /></span>
+          </li>
+          {otherTracks.length > 0 ? 
+              otherTracks.map((user, index) => (
+                <li>
+                  <StreamPlayer audioTrack={user.audioTrack} videoTrack={user.videoTrack} uid={user.uid} showUid={true} key={index} onChangeMainTrack={onChangeMainTrack} />
+                </li>
+              ))
+              :
+              <></>
+            }
+        </ul>
+      </div>
 
-        <div className="header__container">
-            <div className="logo">
-                <img src={new_owake_logo_white} alt="" />
-            </div>
-            <div className="navbar">
-                <div className="setting__icon">
-                    <img src={setting} alt="" onClick={onUseSetting} />
-                </div>
-            </div>
-        </div>
-
-          <div className="body__container">
-              <div className="body__video__box">
-                  <div className="left__box">
-                      <div className="display__box">
-                      <StreamPlayer videoTrack={localVideoTrack} type='local' uid={localUid} showUid={true} />
-                      {remoteUsers.map((user) => (
-                          <div key={user.uid}>
-                              <StreamPlayer audioTrack={user.audioTrack} videoTrack={user.videoTrack} type='remote' uid={user.uid} showUid={true} />
-                          </div>
-                      ))}
-                      </div>
-                  </div>
-                  <div className="right__box">
-
-                    <Chatting useSetting={useSetting} onUseSetting={onUseSetting} />
-
-                  </div>
-                </div>
-            </div>
-        </div>
-
-        
-
-        <div className="footer__container">
-          <div className="footer__navigator">
-              <div className="empty__footer"></div>
-
-              <div className="navigator__icons">
-                  <div className="icons icon__mic">
-                      <img src={useMic ? owake_icon_mic : disable_mic} alt="" onClick={onMic} />
-                      <p>MIC</p>
-                  </div>
-                  <div className="icons icon__cam">
-                      <img src={useVideocam ? owake_icon_cam : disable_cam} alt="" onClick={onVideocam} />
-                      <p>CAM</p>
-                  </div>
-                  <div className="icons icon__share">
-                      <img src={sharing ? owake_icon_share : disable_share} alt="" onClick={onShareScreen} />
-                      <p>SHARE SCREEN</p>
-                  </div>
-                  <div className="icons icon__close">
-                      <img src={owake_icon_close} alt="" onClick={onLeaveChannel} />
-                      <p>LEAVE</p>
-                  </div>
-              </div>
-
-              <div className="empty__footer"></div>
+      <div class="main">
+        <div class="topbar">
+          <div class="toggle" onClick={toggleMenu}>
+            <IonIcon className="menuOutline" icon={menuOutline}></IonIcon>
+          </div>
+          
+          <div class="setting">
+            <img src={setting} alt="" onClick={onUseSetting} />
           </div>
         </div>
-     </>
+
+        <div class="track">
+          <div class="videoTrack">
+            {mainTrack.length > 0 ?
+              <StreamPlayer videoTrack={mainTrack[0].videoTrack} uid={mainTrack[0].uid} showUid={true} onChangeMainTrack={onChangeMainTrack} />
+              :
+              currentTrack.length > 0 ?
+                <StreamPlayer videoTrack={currentTrack[0].videoTrack} uid={currentTrack[0].uid} showUid={true} onChangeMainTrack={onChangeMainTrack} />
+                :
+                <></>
+            }
+          </div>
+        </div>
+
+        <div class="cardBox">
+          <div class="card">
+            {useMic ? <IonIcon icon={micOutline} onClick={onMic}></IonIcon> : <IonIcon icon={micOffOutline} onClick={onMic}></IonIcon>}
+            <p>MIC</p>
+          </div>
+          <div class="card">
+            {useVideocam ? <IonIcon icon={videocamOutline} onClick={onVideocam}></IonIcon> : <IonIcon icon={videocamOffOutline} onClick={onVideocam}></IonIcon>}
+            <p>CAM</p>
+          </div>
+          <div class="card">
+            {sharing ? <IonIcon icon={shareOutline} onClick={onShareScreen}></IonIcon> : <IonIcon icon={shareOutline} onClick={onShareScreen}></IonIcon>}
+            <p>SHARE SCREEN</p>
+          </div>
+          <div class="card">
+            <IonIcon icon={logOutOutline} onClick={onLeaveChannel}></IonIcon>
+            <p>LEAVE</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
